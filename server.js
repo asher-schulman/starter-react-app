50a01a0db299136a1bf95de5a7829f862370006c
Asher Schulman <asher.schulman@gmail.com>
2023-12-18 20:28:36 -0600

    change from create-react-app to vite

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-
-const express = require('express')
-const path = require("path");
-const app = express()
+import express from "express";
+import path from "path";
+const app = express();
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('build', options))
+let options = {
+	dotfiles: "ignore",
+	etag: false,
+	extensions: ["htm", "html", "css", "js", "ico", "jpg", "jpeg", "png", "svg"],
+	index: ["index.html"],
+	maxAge: "1m",
+	redirect: false,
+};
+app.use(express.static("dist", options));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`React app listening at http://localhost:${port}`)
-})
+	console.log(`React app listening at http://localhost:${port}`);
+});
